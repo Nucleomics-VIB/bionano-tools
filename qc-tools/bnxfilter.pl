06787fc414324f78e83097640f945b84ed0d69b7
Stephane Plaisance <stephane.plaisance@vib.be>
2015-06-13 13:28:37 +0200

    added gz support (in and out)

--- qc-tools/bnxfilter.pl
+++ qc-tools/bnxfilter.pl
@@ -28,9 +28,9 @@ use Getopt::Std;
 ############################
 
 getopts('i:l:x:m:s:n:zh');
-our($opt_i, $opt_l, $opt_x, $opt_m, $opt_s, $opt_n, $opt_z, $opt_h);
+our ( $opt_i, $opt_l, $opt_x, $opt_m, $opt_s, $opt_n, $opt_z, $opt_h );
 
-my $usage="You must provide a BNX file with -i
+my $usage = "You must provide a BNX file with -i
 ## Usage: bnxfilter.pl <-i bnx-file>
 # Additional optional parameters are:
 # <-l min-length in kb (100)>
@@ -45,132 +45,152 @@ my $usage="You must provide a BNX file with -i
 # declare variables
 ####################
 
-my $inputfile = $opt_i || die $usage."\n";
-my $minsize = $opt_l || 100;
-my $minsizeb = $minsize * 1_000;
-my $maxsize = $opt_x || 5000;
-my $maxsizeb = $maxsize * 1_000;
-my $minnck = $opt_n || 6;
-my $maxavgint = $opt_m || 0.6;
-my $minsnr = $opt_s || 3.5;
-my $zipit = defined($opt_z) || undef;
-defined($opt_h) && die $usage."\n";
+my $inputfile = $opt_i          || die $usage . "\n";
+my $minsize   = $opt_l          || 100;
+my $minsizeb  = $minsize * 1_000;
+my $maxsize   = $opt_x          || 5000;
+my $maxsizeb  = $maxsize * 1_000;
+my $minnck    = $opt_n          || 6;
+my $maxavgint = $opt_m          || 0.6;
+my $minsnr    = $opt_s          || 3.5;
+my $zipit     = defined($opt_z) || undef;
+defined($opt_h) && die $usage . "\n";
 
 # open stream from BNX file
 # open FILE, $inputfile or die $!;
-my $FILE = OpenArchiveFile ($inputfile) or die $!;
+my $FILE = OpenArchiveFile($inputfile) or die $!;
 my $outpath = dirname($inputfile);
 
 # remove possible suffixes from filename
-my @sufx=( ".bnx", ".bnx.gzip", ".bnx.gz", ".bnx.zip" );
-my $outbase = basename($inputfile, @sufx);
+my @sufx = ( ".bnx", ".bnx.gzip", ".bnx.gz", ".bnx.zip" );
+my $outbase = basename( $inputfile, @sufx );
 
 # include size limit, max intensity, and snr in file names
-my $outfile = $outpath."/filtered_".$minsize."kb_".$minnck."nck_".
-	$maxavgint."ai_".$minsnr."snr_".$outbase.".bnx";
+my $outfile =
+    $outpath
+  . "/filtered_"
+  . $minsize . "kb_"
+  . $minnck . "nck_"
+  . $maxavgint . "ai_"
+  . $minsnr . "snr_"
+  . $outbase . ".bnx";
 
 if ( defined($zipit) ) {
-	open OUT, " | gzip -c > ".$outfile.".gz" || die $!;
-	} else {
-	open OUT, "> $outfile" || die $!;
-		}
+    open OUT, " | gzip -c > " . $outfile . ".gz" || die $!;
+} else {
+    open OUT, "> $outfile" || die $!;
+}
 
-my $logfile = $outpath."/".$outbase."_".$minsize."kb_".$minnck."nck_".
-	${maxavgint}."ai_".$minsnr."snr_filtered-log.txt";
+my $logfile =
+    $outpath . "/"
+  . $outbase . "_"
+  . $minsize . "kb_"
+  . $minnck . "nck_"
+  . ${maxavgint} . "ai_"
+  . $minsnr
+  . "snr_filtered-log.txt";
 
 open OUT2, "> $logfile" || die $!;
 
 # declare variables
-my $counttot = 0;
-my $goodcount = 0;
-my $countshort = 0;
-my $countlong = 0;
-my $countlight = 0;
+my $counttot    = 0;
+my $goodcount   = 0;
+my $countshort  = 0;
+my $countlong   = 0;
+my $countlight  = 0;
 my $countlowsnr = 0;
 my $countlownck = 0;
-my $first = 1;
+my $first       = 1;
 
 ################################
 # parse data and store in array
 ################################
 
-while (my $line = <$FILE>) {
-	# check top line for "# BNX File Version:	1.2"
-	if ($first == 1) {
-		if ($line !~ /#\ BNX\ File\ Version:/) {
-		die "$line\n This does not seem to be a bnx file";
-			}
-		$first = 0;
-		}
+while ( my $line = <$FILE> ) {
 
-	# header block
-	if ($line =~ /^#/) {
-		# handle min-size change
-		if ($line =~ /^# Min Molecule Length.*$/){
-				print OUT "# Min Molecule Length (Kb):\t".$minsize."\n";
-			} else {
-				print OUT $line;
-			}
-		next;
-		}
+    # check top line for "# BNX File Version:	1.2"
+    if ( $first == 1 ) {
+        if ( $line !~ /#\ BNX\ File\ Version:/ ) {
+            die "$line\n This does not seem to be a bnx file";
+        }
+        $first = 0;
+    }
 
-	# entering data part
-	# load four lines in @data
-	my @data=(); # store current four lines of data
+    # header block
+    if ( $line =~ /^#/ ) {
 
-	# test data consistency
-	$line =~ /^0/ or die "aborted, does not seem to be a valid bnx format";
-	$counttot++;
-	push @data, $line;
+        # handle min-size change
+        if ( $line =~ /^# Min Molecule Length.*$/ ) {
+            print OUT "# Min Molecule Length (Kb):\t" . $minsize . "\n";
+        } else {
+            print OUT $line;
+        }
+        next;
+    }
 
-	## 0	1	94694.1 ...
-	# 1	504.4	3008.2 ...
-	# QX11	1.0645	1.3571 ...
-	# QX12	0.0771	0.0778 ...
+    # entering data part
+    # load four lines in @data
+    my @data = ();    # store current four lines of data
 
-	# read three more lines
-	for (my $d=1; $d<4; $d++) {
-		$line = <$FILE> || die "premature end of file";
-		push @data, $line;
-		}
+    # test data consistency
+    $line =~ /^0/ or die "aborted, does not seem to be a valid bnx format";
+    $counttot++;
+    push @data, $line;
 
-	# spit and filter 0-line data
-	my $zerol=$data[0];
-	chomp($zerol);
-	my (undef, undef, $Length, $AvgIntensity, $snr, $nck, undef) =
-		split(/\t/, $zerol);
+    ## 0	1	94694.1 ...
+    # 1	504.4	3008.2 ...
+    # QX11	1.0645	1.3571 ...
+    # QX12	0.0771	0.0778 ...
 
-	# filter
-	if (($Length >= $minsizeb)
-			&& ($Length <= $maxsizeb)
-			&& ($AvgIntensity <= $maxavgint)
-			&& ($snr >= $minsnr)
-			&& ($nck >= $minnck)) {
-		# good record
-		$goodcount++;
-		foreach(@data) { print OUT "$_"; }
-	} else {
-		# too short
-		if ($Length < $minsizeb) {
-			$countshort++;
-			}
-		# too long
-		if ($Length > $maxsizeb) {
-			$countlong++;
-			}
-		# too fluorescent
-		if ($AvgIntensity > $maxavgint) {
-			$countlight++;
-			}
-		# low snr
-		if ($snr < $minsnr) {
-			$countlowsnr++;
-			}
-		# low label frequency
-		if ($nck < $minnck) {
-			$countlownck++;
-			}
-	}
+    # read three more lines
+    for ( my $d = 1; $d < 4; $d++ ) {
+        $line = <$FILE> || die "premature end of file";
+        push @data, $line;
+    }
+
+    # spit and filter 0-line data
+    my $zerol = $data[0];
+    chomp($zerol);
+    my ( undef, undef, $Length, $AvgIntensity, $snr, $nck, undef ) =
+      split( /\t/, $zerol );
+
+    # filter
+    if (    ( $Length >= $minsizeb )
+         && ( $Length <= $maxsizeb )
+         && ( $AvgIntensity <= $maxavgint )
+         && ( $snr >= $minsnr )
+         && ( $nck >= $minnck ) )
+    {
+        # good record
+        $goodcount++;
+        foreach (@data) { print OUT "$_"; }
+    } else {
+
+        # too short
+        if ( $Length < $minsizeb ) {
+            $countshort++;
+        }
+
+        # too long
+        if ( $Length > $maxsizeb ) {
+            $countlong++;
+        }
+
+        # too fluorescent
+        if ( $AvgIntensity > $maxavgint ) {
+            $countlight++;
+        }
+
+        # low snr
+        if ( $snr < $minsnr ) {
+            $countlowsnr++;
+        }
+
+        # low label frequency
+        if ( $nck < $minnck ) {
+            $countlownck++;
+        }
+    }
 }
 
 # take care of handles neetly
@@ -181,14 +201,17 @@ close OUT;
 # print summary
 ##################
 
-print OUT2 join("\t", "minLength",  "maxLength", "minLabelCount",
-	"maxAvgIntensity", "minSNR",
-	"tot-molecules", "retained", "size-reject_short", "size-reject_long",
-	"lowNCK-reject", "AvgInt-reject", "lowSNR-reject")."\n";
-print OUT2 join("\t", $minsize, $maxsize, $minnck,
-	$maxavgint, $minsnr,
-	$counttot, $goodcount, $countshort, $countlong,
-	$countlownck, $countlight, $countlowsnr)."\n";
+print OUT2 join( "\t",
+                 "minLength",       "maxLength",         "minLabelCount",
+                 "maxAvgIntensity", "minSNR",            "tot-molecules",
+                 "retained",        "size-reject_short", "size-reject_long",
+                 "lowNCK-reject",   "AvgInt-reject",     "lowSNR-reject" )
+  . "\n";
+print OUT2 join( "\t",
+                 $minsize,   $maxsize,     $minnck,     $maxavgint,
+                 $minsnr,    $counttot,    $goodcount,  $countshort,
+                 $countlong, $countlownck, $countlight, $countlowsnr )
+  . "\n";
 
 close OUT2;
 
@@ -197,20 +220,20 @@ exit 0;
 ##############
 #### Subs ####
 
-sub OpenArchiveFile
-{
-    # $Filename passed in, handle to file passed out
-    my $File = shift; # filename
-    my $FH; # file handle
+sub OpenArchiveFile {
 
-    if ($File =~ /.bnx$/) {
-		open ($FH, "cat $File | ") or die ("$!: can't open file $File");
-		} elsif ($File =~ /.bnx.zip$/) {
-				open ($FH, "unzip -p $File | ") or die ("$!: can't open file $File");
-			} elsif ($File =~ /(.bnx.gzip|.bnx.gz)$/) {
-					open ($FH, "gzip -dc $File | ") or die ("$!: can't open file $File");
-				} else {
-						die ("$!: the file $File does seem to be a 'bnx' file");
-					}
+    # $Filename passed in, handle to file passed out
+    my $File = shift;    # filename
+    my $FH;              # file handle
+
+    if ( $File =~ /.bnx$/ ) {
+        open( $FH, "cat $File | " ) or die("$!: can't open file $File");
+    } elsif ( $File =~ /.bnx.zip$/ ) {
+        open( $FH, "unzip -p $File | " ) or die("$!: can't open file $File");
+    } elsif ( $File =~ /(.bnx.gzip|.bnx.gz)$/ ) {
+        open( $FH, "gzip -dc $File | " ) or die("$!: can't open file $File");
+    } else {
+        die("$!: the file $File does seem to be a 'bnx' file");
+    }
     return $FH;
 }
