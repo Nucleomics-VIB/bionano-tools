f5449a08cfb0170805991a10f8c183faf9f367a8
Stephane Plaisance <stephane.plaisance@vib.be>
2016-03-04 16:21:09 +0100

    edit

--- qc-tools/bnxfilter_repeats.sh
+++ qc-tools/bnxfilter_repeats.sh
@@ -17,14 +17,16 @@ usage='# Usage: bnxfilter_repeats.sh
 #		-i <input (bnx file)>
 #		[opt -t <stretch tolerance|0.1>]
 #		[opt -m <min Repeat Units|5>]
-#		[opt -c <choice (1/2/3)|1>]'
+#		[opt -c <choice (1/2/3)|1>]
+#		[opt -l <keep log>]'
 
-while getopts "i:t:m:c:h" opt; do
+while getopts "i:t:m:c:lh" opt; do
   case $opt in
     i) input=${OPTARG} ;;
     t) tolerance=${OPTARG} ;;
     m) minele=${OPTARG} ;;
     c) choice=${OPTARG} ;;
+    l) log=TRUE ;;
     h) echo "${usage}" >&2; exit 0 ;;
     \?) echo "Invalid option: -${OPTARG}" >&2; exit 1 ;;
     *) echo "this command requires arguments, try -h" >&2; exit 1 ;;
@@ -38,6 +40,9 @@ timestamp=$(date +%s)
 tol=${tolerance:-0.1}
 min=${minele:-5}
 act=${choice:-1}
+if [ ${log} ]; then
+	keeplog="-stderr -stdout"
+fi
 
 # test if minimal arguments were provided
 if [ -z "${input}" ]
@@ -62,7 +67,7 @@ case ${act} in
 	[1]) tag="nonrepeats"; ;;
 	[2]) tag="repeats"; ;;
 	[3]) tag="repeatmask"; ;;
-	*) echo "not a valid action (1,2,3)"; exit 1 ;;
+	*) echo "invalid value for -c (1,2,3)"; exit 1 ;;
 esac;
 
 # build command
@@ -80,7 +85,9 @@ esac;
 #		2 = output only maps which contain any repeats,
 #		3 = output all maps but with all repeats masked (ie, labels removed)
 #	code also outputs .rmap describing repeats found,
-#   	and repeatStats.txt file summarizing repeat statistics.
+#   	and repeatStats.txt file summarising repeat statistics.
+
+echo "## filtering ${name} for ${choice}:${tag}"
 
 cmd="${TOOLS}/RefAligner \
 	-i ${input} \
@@ -88,23 +95,20 @@ cmd="${TOOLS}/RefAligner \
 	-o ${foldername}/${name} \
 	-simpleRepeatTolerance ${tol} \
 	-simpleRepeatMinEle ${min} \
-	-simpleRepeatStandalone \
-	-stderr"
+	-simpleRepeatStandalone ${keeplog}"
 
-echo "# filtering ${name} for ${choice} : ${tag}"
-echo -e "# command: ${cmd}"
-#eval ${cmd}
+echo -e "## command: ${cmd}"
+eval ${cmd}
 
 # did the command succeed?
 if [ $? -eq 0 ]; then
-	echo "# results were saved to ${foldername}/${name}_${tag}.bnx"
-	echo "# additional info was written to two files:"
-	echo -e "# \'${foldername}/${name}.rmap\' and \'${foldername}/${name}_repeatStats.txt\'"
+	echo "## results were saved to ${foldername}/${name}_${tag}.bnx"
+	echo "## additional info was written to ${foldername}/${name}.rmap and ${foldername}/${name}_repeatStats.txt"
 	echo
 	if [ -f "${foldername}/${name}_repeatStats.txt" ]; then
 		echo
-		echo "# summary"
-		cat ${foldername}/${name}_repeatStats.txt
+		echo "## summary"
+		grep -v "^#" ${foldername}/${name}_repeatStats.txt | column -s ":" -t
 	fi
 else
     echo "# the command failed!"
